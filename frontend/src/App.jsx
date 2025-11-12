b59c6ace1fb35e2b023c6cfcfaa54535aab9f014
Rahees <rahees@Raheess-MacBook-Pro.local>
2025-11-12 15:48:51 +0530

    env added in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,12 @@ import axios from "axios";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 
-const API_URL = "http://localhost:8080/todos";
+//const API_URL = "http://localhost:8080/todos";
+
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_URL = `${API_BASE_URL}/todos`;
+
 
 function App() {
   const [todos, setTodos] = useState([]);
